3a634f250528ff9feb8332cb0802688d628d1d96
mikael.thorsson <mikaelt@mikaelthorsson.local>
2017-09-08 08:57:21 +0200

    Added project files

---
+++ src/test/java/se/cygni/codechallenge/elevator/IntegrationTest.java
@@ -0,0 +1,25 @@
+package se.cygni.codechallenge.elevator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import se.cygni.codechallenge.elevator.config.ElevatorApplication;
+
+/**
+ * Boiler plate test class to get up and running with a test faster.
+ *
+ * @author Sven Wesley
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ElevatorApplication.class)
+public class IntegrationTest {
+
+    @Test
+    public void simulateAnElevatorShaft() {
+
+    }
+
+}
